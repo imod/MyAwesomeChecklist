e31741fa7d592884a9f7efb6e83feb59612cb36f
imod <domi@fortysix.ch>
2014-06-09 08:56:05 +0200

    initial

---
+++ MyAwesomeChecklistTests/MyAwesomeChecklistTests.swift
@@ -0,0 +1,35 @@
+//
+//  MyAwesomeChecklistTests.swift
+//  MyAwesomeChecklistTests
+//
+//  Created by Dominik on 07/06/14.
+//  Copyright (c) 2014 Dominik. All rights reserved.
+//
+
+import XCTest
+
+class MyAwesomeChecklistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
